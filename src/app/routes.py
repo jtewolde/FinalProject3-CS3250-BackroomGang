ffc2a664bf648b42c203edec38ceb43ff6cd7ff8
Joseph <jtewold1@msudenver.edu>
2023-12-05 22:21:14 -0700

    Fully Implemented black-box testing program and added hyperlink for provided video url

--- src/app/routes.py
+++ src/app/routes.py
@@ -8,7 +8,6 @@ Group Name: Backroom Gang
 
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db
-# Make the forms.py file and import the forms from it
 from app.forms import RegistrationForm, LoginForm, PlaylistForm, VideoForm
 from app.models import User, Playlist, Video
 from flask_login import login_user, current_user, logout_user, login_required
@@ -23,7 +22,6 @@ def show_index():
     return render_template('index.html')
 
 
-# TODO 1: Implement User Story 1: Registration and Signing up (USE SIGNUP ROUTE FROM PREVIOUS PROJECTS BUT MODIFY IT TO FIT THIS PROJECT)
 @app.route('/users/signup', methods=['GET', 'POST'])
 def users_signup():
     """ This function is meant for implementing the signup route for the application """
@@ -53,18 +51,18 @@ def users_signup():
     return render_template('signup.html', title='Sign Up', form=form)
 
 
-# TODO 2: Implement User Story 2: Login and Signing in (USE LOGIN ROUTE FROM PREVIOUS PROJECTS BUT MODIFY IT TO FIT THIS PROJECT)
 @app.route('/users/login', methods=['GET', 'POST'])
 def users_login():
     """ This function is meant for implementing the login route for the application """
     form = LoginForm()
     if form.validate_on_submit():  # If the form is validated
-        user = User.query.filter_by(id=form.id.data).first() # Get the user from the database
-        if user: # If the user exists
-            passwd = form.passwd.data # Get the password from the form
-            salt_passwd = bcrypt.gensalt() # Generate a salt for the password
-            hashed_passwd = bcrypt.hashpw(passwd.encode( 
-                'utf-8'), salt_passwd) # Hash the password
+        # Get the user from the database
+        user = User.query.filter_by(id=form.id.data).first()
+        if user:  # If the user exists
+            passwd = form.passwd.data  # Get the password from the form
+            salt_passwd = bcrypt.gensalt()  # Generate a salt for the password
+            hashed_passwd = bcrypt.hashpw(passwd.encode(
+                'utf-8'), salt_passwd)  # Hash the password
             if bcrypt.checkpw(passwd.encode('utf-8'), user.passwd):
                 login_user(user)
                 flash('You have successfully logged in!', 'success')
@@ -84,67 +82,78 @@ def users_signout():
     flash('You were successfully logged out!')
     return redirect(url_for('show_index'))
 
-# TODO 3: Implement User Story 2: View Playlists (Refer to one of the previous homeworks when viewing all of the invoices for this)
-
 
 @app.route('/playlists', methods=['GET', 'POST'])
 @login_required
 def view_playlists():
     """ This function is meant for implementing the view playlists route for the application """
 
-    playlists = Playlist.query.filter_by(users_id=current_user.id).all() # Get all of the playlists from the database
-    return render_template('playlists.html', title='Playlists', playlists=playlists) # Render the playlists.html template and pass the playlists to it
-
-# TODO 4: Implement User Story 3: Create a Playlist (Refer to one of the previous homeworks when creating an invoice for this)
+    # Get all of the playlists from the database
+    playlists = Playlist.query.filter_by(users_id=current_user.id).all()
+    # Render the playlists.html template and pass the playlists to it
+    return render_template('playlists.html', title='Playlists', playlists=playlists)
 
 
 @app.route('/playlists/create', methods=['GET', 'POST'])
 @login_required
 def create_playlists():
     """ This function is meant for implementing the create playlist route for the application """
-    form = PlaylistForm() # Create an instance of the PlaylistForm class
-    if form.validate_on_submit(): # If the form is validated
-        playlist = Playlist(name=form.name.data, description=form.description.data, creation_date=form.creation_date.data, creator_name=form.creator_name.data, quantity=form.quantity.data, users_id=current_user.id)
-        db.session.add(playlist) # Add the playlist to the database
+    form = PlaylistForm()  # Create an instance of the PlaylistForm class
+    if form.validate_on_submit():  # If the form is validated
+        playlist = Playlist(name=form.name.data, description=form.description.data, creation_date=form.creation_date.data,
+                            creator_name=form.creator_name.data, quantity=form.quantity.data, users_id=current_user.id)
+        db.session.add(playlist)  # Add the playlist to the database
         db.session.commit()
         flash('You have successfully created a playlist!', 'success')
         return redirect(url_for('view_playlists'))
     return render_template('add_playlist.html', title='Create Playlist', form=form)
-
-# TODO 5: Implement User Story 3: View Videos in a Playlist (Refer to one of the previous homeworks when viewing all of the items for this)
 
 
 @app.route('/playlists/<playlist_id>/videos', methods=['GET', 'POST'])
 @login_required
 def view_playlists_videos(playlist_id):
     """ This function is meant for implementing the view videos in playlist route for the application """
-    playlist = Playlist.query.filter_by(id=playlist_id).first() # Get the playlist from the database
-    videos = Video.query.filter_by(playlist_id=playlist_id).all() # Get all of the videos from the database
-    return render_template('videos.html', title='Videos', playlist=playlist, videos=videos) # Render the videos.html template and pass the playlist and videos to it
-
-# TODO 6: Implement User Story 4: Adding videos to a playlist
+    playlist = Playlist.query.filter_by(
+        id=playlist_id).first()  # Get the playlist from the database
+    # Get all of the videos from the database
+    videos = Video.query.filter_by(playlist_id=playlist_id).all()
+    # Render the videos.html template and pass the playlist and videos to it
+    return render_template('videos.html', title='Videos', playlist=playlist, videos=videos)
 
 
 @app.route('/playlists/<playlist_id>/videos/add', methods=['GET', 'POST'])
 @login_required
-def add_playlists_video():
-    """ This function is meant for implementing the add videos to playlist route for the application """
-    pass
-
-# TODO 7: Implement User Story 5: Deleting videos from a playlist(Refer to Homework 3 when deleting an invoice item for this)
+def add_playlists_video(playlist_id):
+    """ This function is meant for implementing the add a video to playlist route for the application """
+    form = VideoForm()
+    if form.validate_on_submit():
+        video = Video(name=form.name.data, url=form.url.data,
+                      length=form.length.data, playlist_id=playlist_id)
+        db.session.add(video)
+        db.session.commit()
+        flash('You have added a video to the playlist!')
+        return redirect(url_for('view_playlists_videos', playlist_id=playlist_id))
+    return render_template('add_video.html', title='Add Video', form=form, playlist_id=playlist_id)
 
 
 @app.route('/playlists/<playlist_id>/<video_id>/delete', methods=['GET', 'POST'])
 @login_required
-def delete_playlists_video():
+def delete_playlists_video(playlist_id, video_id):
     """ This function is meant for implementing the delete a video from playlist route for the application """
-    pass
-
-# TODO 8: Implement User Story 6: Deleting a playlist (Refer to Homework 3 when deleting an invoice for this)
+    video = Video.query.filter_by(
+        id=video_id).first()  # Get the video from the database
+    db.session.delete(video)
+    db.session.commit()
+    flash('You have deleted a video from the playlist!')
+    return redirect(url_for('view_playlists_videos', video_id=video_id, playlist_id=playlist_id))
 
 
 @app.route('/playlists/<playlist_id>/delete', methods=['GET', 'POST'])
 @login_required
-def delete_playlists():
+def delete_playlists(playlist_id):
     """ This function is meant for implementing the delete playlist route for the application """
-    pass
+    playlist = Playlist.query.filter_by(id=playlist_id).first()
+    db.session.delete(playlist)
+    db.session.commit()
+    flash('You have deleted a playlist!')
+    return redirect(url_for('view_playlists'))
